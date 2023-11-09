97d2f09a8bc3843d8706ed6f9a30022ff7940434
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2023-11-09 18:33:43 +0100

    Update docker build

--- docker_build.sh
+++ docker_build.sh
@@ -13,8 +13,8 @@ docker compose -f "${BASE_DIR}/toolchains/docker-compose.arm.yml" build --build-
 BUILD_CMD="cd /out && rm -rf /out/* && cmake -DCMAKE_TOOLCHAIN_FILE=/build/toolchains/armel.cmake -DBUILD_SHARED=ON -DBUILD_SUFFIX=armel ${CMAKE_EXTRA} /build && make clean all"
 docker compose -f "${BASE_DIR}/toolchains/docker-compose.arm.yml" run --rm build_armel sh "-c" "${BUILD_CMD}"
 
-BUILD_CMD="cd /out && rm -rf /out/* && cmake -DCMAKE_TOOLCHAIN_FILE=/build/toolchains/armel.cmake -DBUILD_SHARED=ON -DBUILD_SUFFIX=armhf ${CMAKE_EXTRA} /build && make clean all"
+BUILD_CMD="cd /out && rm -rf /out/* && cmake -DCMAKE_TOOLCHAIN_FILE=/build/toolchains/armhf.cmake -DBUILD_SHARED=ON -DBUILD_SUFFIX=armhf ${CMAKE_EXTRA} /build && make clean all"
 docker compose -f "${BASE_DIR}/toolchains/docker-compose.arm.yml" run --rm build_armhf sh "-c" "${BUILD_CMD}"
 
-BUILD_CMD="cd /out && rm -rf /out/* && cmake -DCMAKE_TOOLCHAIN_FILE=/build/toolchains/armel.cmake -DBUILD_SHARED=ON -DBUILD_SUFFIX=aarch64 ${CMAKE_EXTRA} /build && make clean all"
+BUILD_CMD="cd /out && rm -rf /out/* && cmake -DCMAKE_TOOLCHAIN_FILE=/build/toolchains/aarch64.cmake -DBUILD_SHARED=ON -DBUILD_SUFFIX=aarch64 ${CMAKE_EXTRA} /build && make clean all"
 docker compose -f "${BASE_DIR}/toolchains/docker-compose.arm.yml" run --rm build_aarch64 sh "-c" "${BUILD_CMD}"
